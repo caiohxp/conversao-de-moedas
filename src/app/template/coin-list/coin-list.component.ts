1769e1d8ef4f8c51d91fd0a40ac729b98ddcc891
caiohxp <caiocesar.ccomp@gmail.com>
2023-01-22 23:32:26 -0300

    up coin list paginacao

--- src/app/template/coin-list/coin-list.component.ts
+++ src/app/template/coin-list/coin-list.component.ts
@@ -14,7 +14,6 @@ export class CoinListComponent implements OnInit {
   arraySymbols: Array<any>;
   arraySymbol: Symbol[];
   displayedColumns = ['code', 'description'];
-  filtro = false;
   tipoDeFiltro: Filtro;
   constructor(private http: HttpClient) {
 
@@ -25,74 +24,28 @@ export class CoinListComponent implements OnInit {
 
   }
   SemFiltro() {
-    this.filtro = false;
   }
   filtrarSigla() {
-    this.filtro = true;
-    this.tipoDeFiltro = Filtro.Sigla;
-
-    console.log(this.tipoDeFiltro);
-
   }
   filtrarNome() {
-    this.filtro = true;
-    this.tipoDeFiltro = Filtro.Nome;
+    
   }
   filtrarMoedas(symbol: any[]) {
     const input = (<HTMLInputElement>document.querySelector('#input-filtro')).value;
     const select = document.querySelectorAll('.form-select');
     const selectCampo = (<HTMLInputElement>select[0]).value;
     const selectOrdem = (<HTMLInputElement>select[1]).value;
+    const selectPaginacao = parseInt((<HTMLInputElement>select[2]).value);
+    
     let array: any[];
     if (selectCampo == 's')
-      array = symbol.filter((s, i) => s.code.includes(input.toUpperCase()) && s.code.charAt(0) === input.toUpperCase().charAt(0)).reverse();
+      array = symbol.filter((s, i) => s.code.includes(input.toUpperCase()) && s.code.charAt(0) === input.toUpperCase().charAt(0) && i < selectPaginacao);
     else if (selectCampo == 'n')
-      array = symbol.filter(s => s.description.toLowerCase().includes(input.toLowerCase()));
-    else array = symbol;
+      array = symbol.filter((s, i) => s.description.toLowerCase().includes(input.toLowerCase()) && i < selectPaginacao);
+    else array = symbol.filter((s, i) => i < selectPaginacao);
     if(selectOrdem == 'c') array.sort();
     else if(selectOrdem == 'd') array.sort().reverse();
 
     return array;
   }
-  callSymbols(): Object[] {
-    var requestURL = 'https://api.exchangerate.host/symbols';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-
-    request.onload = function () {
-      var response = request.response;
-      return Object.values(response.symbols);
-    }
-    return Object.values(request.response);
-  }
 }
-// const getSymbols = () => {
-
-//   var requestURL = 'https://api.exchangerate.host/symbols';
-//   var request = new XMLHttpRequest();
-//   request.open('GET', requestURL);
-//   request.responseType = 'json';
-//   request.send();
-
-//   request.onload = function () {
-//     return Object.values(request.response.symbols);
-//   }
-// }
-// const getSymbols = async () => {
-//   fetch("https://api.exchangerate.host/symbols")
-//     .then((response) => response.json())
-//     .then((responseJson) => {
-//       return Object.values(responseJson.symbols)
-//     })
-
-// }
-// const getContatos = () => {
-//   fetch("https://api.exchangerate.host/symbols")
-//     .then((response) => response.json())
-//     .then((responseJson) => {
-//       return responseJson
-//     })
-// }
-// console.log(getSymbols)
